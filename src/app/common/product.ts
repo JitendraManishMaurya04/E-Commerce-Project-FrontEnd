12bd28ad62a73a6ffdbc46ead9a36b209f257cf1
Jitendra Manish <jmanishm7@gmail.com>
2021-07-25 23:43:55 +0530

    My First Innitial Angular Code Commit

---
+++ src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+
+    sku: string | undefined;
+	name: string | undefined;
+	description: string | undefined;
+	unitPrice: number | undefined;
+    imageUrl: string | undefined ;
+    active: boolean | undefined;
+    unitsInStock: number | undefined;
+    dateCreated: Date | undefined;
+    lastUpdated: Date | undefined;
+
+}
