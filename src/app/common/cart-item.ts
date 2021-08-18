4889655cc325eca5a5beb56e73267d833d303471
Jitendra Manish <jmanishm7@gmail.com>
2021-08-18 15:43:17 +0530

    Added Logic for Shopping Cart status

---
+++ src/app/common/cart-item.ts
@@ -0,0 +1,21 @@
+import { Product } from "./product";
+
+export class CartItem {
+
+    id : number | undefined;
+	name: string | undefined;
+	unitPrice: number | any;
+    imageUrl: string | undefined;
+
+    quantity: number;
+    
+    constructor(product: Product){
+        this.id= product.id;
+        this.name= product.name;
+        this.unitPrice= product.unitPrice;
+        this.imageUrl= product.imageUrl;
+
+        this.quantity= 1;
+    }
+
+}
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartItem } from '../common/cart-item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+
+  //Subject is a Sub-Class of Observable to publish events
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+  addToCart(theCartItem: CartItem){
+
+    //Check if the Item is already present in the Cart
+    let alreadyExistsInCart: boolean = false;
+
+    if(this.cartItems.length > 0){
+      //Find the Item in the Cart based on Item-ID
+      for(let tempCartItem of this.cartItems){
+        if(tempCartItem.id === theCartItem.id){
+          alreadyExistsInCart = true;
+          tempCartItem.quantity++;
+          break;
+        }
+      }
+
+    }
+
+    if(alreadyExistsInCart === false){
+      this.cartItems.push(theCartItem);
+    }
+
+    this.computeCartTotals();
+  }
+
+  computeCartTotals() {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for(let currentCartItem of this.cartItems){
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+      totalQuantityValue += currentCartItem.quantity;
+    }
+
+    //Publish the new values ... All Subscribers will receive the new data
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    console.log(`TOTAL CART VALUE: ${totalPriceValue}, ${totalQuantityValue}`);
+    
+  }
+}
